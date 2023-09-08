18c07bce65f8a5067e649986b43f169cf867e419
Michael Anderjaska <m.anderjaska@gmail.com>
2023-09-07 17:05:57 -0700

    how I would do this

--- src/Sections/LoginForm.tsx
+++ src/Sections/LoginForm.tsx
@@ -1,37 +1,64 @@
-import Username from "../Components/Username.tsx";
-import React, { useState } from "react";
-import Password from "../Components/Password.tsx";
+import React, { useState, useEffect } from "react";
+import { validatePassword, validatePhoneOrEmail } from "../utils.ts";
 
 interface LoginFormProps {
   submit: (userData: { username: string; password: string }) => void;
 }
 
-function LoginForm({ submit }: LoginFormProps) {
-  const [usernameValid, setUsernameValid] = useState(false);
-  const [passwordValid, setPasswordValid] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const LoginForm: React.FC<LoginFormProps> = ({ submit }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // these state variables are changing every time the user types, which is not great
+  // extra credit: what function could you use to reduce re-rendering?
+  // hint: It's part of the lodash library
+
+  // regex validation is pretty cheap computationally, so it's fine to do this
+  // extra credit: what hook could you use if validation was expensive?
+  const isPasswordValid = validatePassword(password);
+  const isUsernameValid = validatePhoneOrEmail(username);
+  const isFormValid = isPasswordValid && isUsernameValid;
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault(); // prevent default form submission
-    if (usernameValid && passwordValid) {
+    if (isFormValid) {
       submit({ username, password });
     }
   };
 
+  const showUsernameError = !isUsernameValid && username.length > 0;
+  const showPasswordError = !isPasswordValid && password.length > 0;
+
   return (
-    <form onSubmit={handleSubmit}>
-      <Username
-        setValidity={setUsernameValid}
-        onValueChange={setUsername}
-      />
-      <Password
-        setValidity={setPasswordValid}
-        onValueChange={setPassword}
-      />
-      <button disabled={!usernameValid || !passwordValid}>Submit</button>
-    </form>
-  )
-}
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "16px",
+          width: "25vw",
+        }}
+      >
+        {/* Mention that if you had more time, you would show better error messages */}
+        {/* how else would you improve the UX? */}
+        {showUsernameError && <p style={{ color: "red" }}>Invalid username</p>}
+        <input
+          placeholder="Phone or Email"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          disabled={isFormValid}
+        />
+        {showPasswordError && <p style={{ color: "red" }}>Invalid password </p>}
+        <input
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          disabled={isFormValid}
+        />
+        <button disabled={isFormValid}>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default LoginForm;
